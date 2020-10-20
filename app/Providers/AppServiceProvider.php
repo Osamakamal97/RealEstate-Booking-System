3f4232e5e918b4a20d6bdfcb9ab144bbeced4caf
OsamaKJ <osmaka1997@gmail.com>
2020-10-20 11:29:38 +0300

    fix permissions directives2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,55 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        
+       Blade::directive('role', function ($arguments) {
+            list($role, $guard) = explode(',', $arguments.',');
+
+            return "<?php if(auth({$guard})->check() && auth({$guard})->user()->hasRole({$role})): ?>";
+        });
+       Blade::directive('elserole', function ($arguments) {
+            list($role, $guard) = explode(',', $arguments.',');
+
+            return "<?php elseif(auth({$guard})->check() && auth({$guard})->user()->hasRole({$role})): ?>";
+        });
+       Blade::directive('endrole', function () {
+            return '<?php endif; ?>';
+        });
+
+       Blade::directive('hasrole', function ($arguments) {
+            list($role, $guard) = explode(',', $arguments.',');
+
+            return "<?php if(auth({$guard})->check() && auth({$guard})->user()->hasRole({$role})): ?>";
+        });
+       Blade::directive('endhasrole', function () {
+            return '<?php endif; ?>';
+        });
+
+       Blade::directive('hasanyrole', function ($arguments) {
+            list($roles, $guard) = explode(',', $arguments.',');
+
+            return "<?php if(auth({$guard})->check() && auth({$guard})->user()->hasAnyRole({$roles})): ?>";
+        });
+       Blade::directive('endhasanyrole', function () {
+            return '<?php endif; ?>';
+        });
+
+       Blade::directive('hasallroles', function ($arguments) {
+            list($roles, $guard) = explode(',', $arguments.',');
+
+            return "<?php if(auth({$guard})->check() && auth({$guard})->user()->hasAllRoles({$roles})): ?>";
+        });
+       Blade::directive('endhasallroles', function () {
+            return '<?php endif; ?>';
+        });
+
+       Blade::directive('unlessrole', function ($arguments) {
+            list($role, $guard) = explode(',', $arguments.',');
+
+            return "<?php if(!auth({$guard})->check() || ! auth({$guard})->user()->hasRole({$role})): ?>";
+        });
+       Blade::directive('endunlessrole', function () {
+            return '<?php endif; ?>';
+        });
     }
 
     /**
